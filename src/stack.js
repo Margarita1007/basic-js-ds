783aba6d2e250db9967396c0b1a9fef47584dcce
Margarita1007 <m.kenshova@mail.ru>
2022-05-02 23:40:31 +0400

    feat: complete task

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Stack {
+  constructor () {
+    this.st = [];
+  }
+  push(element) {
+    this.st.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.st.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.st[this.st.length-1];
   }
 }
 
