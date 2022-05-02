007b94a7940470c47953762cabfc652e2f0206d7
Margarita1007 <m.kenshova@mail.ru>
2022-05-02 21:12:35 +0400

    feat: one task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,135 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ class ListNode {
+    constructor(x) {
+       this.value = x;
+       this.next = null;
+     }
+   }
+
+/*
+ function arrayToList(array) {
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+      list = { value: array[i], next: list };
+  }
+  return list;
+} */
+ 
+
+/*
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
 }
+
+*/
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+
+
+
+function removeKFromList(l, k) {
+
+  let head = l;
+
+  function countPositions(k) { // посчитаем, сколько раз нужно прогнать поиск позиции и удаление элемента
+      let current = l;
+      let positions = 0;
+
+      while (current) {
+          if (current.value === k) {
+              positions++;
+          }
+          current = current.next;
+      }
+      return positions;
+  }
+
+  let repeater = countPositions(k); // количество прогонок
+
+  function searchPosition(k) { // поиск позиции для удаления элемента
+      let current = head;
+      let position = 0;
+
+      while (current) {
+        if (current.value === k) {
+          return position;
+        }
+
+        current = current.next;
+        position++;
+      }
+      //return position;
+  }
+
+  function removeAt(position) {
+      let current = head;
+      if (position === 0) {
+        head = current.next;
+      } else {
+        let prev = null;
+        let index = 0;
+
+        while (index < position) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+
+        prev.next = current.next;
+      }
+  }
+
+  for (let i=1; i <= repeater; i ++) {
+      let position = searchPosition(k);
+      removeAt(position);
+  }
+
+  return head;
+ 
+}
+
+
+  let  list1 = [3, 1, 2, 3, 4, 5];
+  //console.log(convertArrayToList(list1))
+
+  console.log( removeKFromList(list1, 3) )
+   
+
+  
+
+
+/*
+let l = {a:3,
+a: 1,
+a: 2,
+a: 3,
+a: 4,
+a: 5}; //3, 1, 2, 3, 4, 5
+
+*/
 
 module.exports = {
   removeKFromList
